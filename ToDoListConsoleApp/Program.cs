14ad04abd11054e5eea4bce598be746311d60194
alair-pires <alair-pires@outlook.com>
2023-12-31 15:18:11 -0300

    Add project files.

---
+++ ToDoListConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TasksModel> tasks = new List<TasksModel>();
+            
+            Console.WriteLine("To do List:");
+
+            foreach(var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+
+        }
+    }
+}
